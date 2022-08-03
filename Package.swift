f7a84560089c010d59e8253824fb394b2ebfa34f
Sam Pettersson <sam@sampettersson.com>
2022-08-03 08:31:41 +0200

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "OdysseyKit",
+    platforms: [
+        .iOS(.v14),
+    ],
+    products: [
+        .library(
+            name: "OdysseyKit",
+            targets: ["OdysseyKit"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "OdysseyKit",
+            path: "OdysseyKit.xcframework"
+        )
+    ]
+)
