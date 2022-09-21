07922ca7f8f967cdf0b17313fb5c1ab64904d174
Sam Pettersson <sam@sampettersson.com>
2022-09-21 20:21:47 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,32 +17,32 @@ let package = Package(
         .binaryTarget(
             name: "OdysseyKit",
             url: "https://github.com/HedvigInsurance/OdysseyKit/releases/download/v0.80.0-beta.4/OdysseyKit.xcframework.zip",// OdysseyKit URL
-            checksum: "2b2457af453b455a59f9c0e6efbfbb668c86363fb48c69c4a4e766a3d8d24776"// OdysseyKit Checksum
+            checksum: "668fb77dd7032f1b81d864489301fdf998ab6b4eb7a688b93c1a5f2fe54307fd"// OdysseyKit Checksum
         ),
         .binaryTarget(
             name: "Odyssey",
             url: "https://github.com/HedvigInsurance/OdysseyKit/releases/download/v0.80.0-beta.4/Odyssey.xcframework.zip",// Odyssey URL
-            checksum: "5cd8f3c693fefe8a492a733a4bee6aafe63219af900890451fa9941d4e42eed0"// Odyssey Checksum
+            checksum: "51695ecc050c8d7db681cbd4d15b80158433103172f65bbf39c8af003346c844"// Odyssey Checksum
         ),
         .binaryTarget(
             name: "KMPNativeCoroutinesCore",
             url: "https://github.com/HedvigInsurance/OdysseyKit/releases/download/v0.80.0-beta.4/KMPNativeCoroutinesCore.xcframework.zip",// KMPNativeCoroutinesCore URL
-            checksum: "f8065f111424817169db9ac81c0740b317d07c0ab00f825a0c061ba5af11253a"// KMPNativeCoroutinesCore Checksum
+            checksum: "431243e9ab0b9d401b6c4e180c59094b12e5f1f750d2a6ffc300358ddaf0440b"// KMPNativeCoroutinesCore Checksum
         ),
         .binaryTarget(
             name: "KMPNativeCoroutinesAsync",
             url: "https://github.com/HedvigInsurance/OdysseyKit/releases/download/v0.80.0-beta.4/KMPNativeCoroutinesAsync.xcframework.zip",// KMPNativeCoroutinesAsync URL
-            checksum: "d84085c4c70d6cf30608556fbefc52a70a0dd531a7aafa63608e827d78f5d43d"// KMPNativeCoroutinesAsync Checksum
+            checksum: "341f966ba1fbff7470c03573989bfa7f3bf469a13f0b54698110272a0e16e483"// KMPNativeCoroutinesAsync Checksum
         ),
         .binaryTarget(
             name: "SVGView",
             url: "https://github.com/HedvigInsurance/OdysseyKit/releases/download/v0.80.0-beta.4/SVGView.xcframework.zip",// SVGView URL
-            checksum: "451ad38d2f14d366c31aaa2e1194fded9b247ff2cd4646dea7956634e035268f"// SVGView Checksum
+            checksum: "5cba130789bc68c2c05c5dc4e4f8df1bf30e2a61bca619e20d935d76daec7a39"// SVGView Checksum
         ),
         .binaryTarget(
             name: "UIPilot",
             url: "https://github.com/HedvigInsurance/OdysseyKit/releases/download/v0.80.0-beta.4/UIPilot.xcframework.zip",// UIPilot URL
-            checksum: "ae3a15fb05443e4a33d269af3c577001f692b3ac4f21f62de1f2c529e8a59a84"// UIPilot Checksum
+            checksum: "b1d08a607ec9239001d15981a177ecd31e2710e78978ac3889ccb23262bc7ab3"// UIPilot Checksum
         ),
         .target(
             name: "OdysseyKitTargets",
