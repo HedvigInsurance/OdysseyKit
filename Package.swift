bc1d80a25fb833aa6dfec76e5392c19e27620b2e
Hedvig iOS <ios@hedvig.com>
2022-09-28 01:38:21 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,12 +17,12 @@ let package = Package(
         .binaryTarget(
             name: "OdysseyKit",
             url: "https://github.com/HedvigInsurance/OdysseyKit/releases/download/v1.1.0-beta.26/OdysseyKit.xcframework.zip",// OdysseyKit URL
-            checksum: "ec93bfeec0e4b7c3960cd94149ffaf376eda15e1d2180944d8d90eea2a566995"// OdysseyKit Checksum
+            checksum: "0238ebe4b3a53c3098ad1b6847baac297b014c42177c294c8156e261373b47ec"// OdysseyKit Checksum
         ),
         .binaryTarget(
             name: "Odyssey",
             url: "https://github.com/HedvigInsurance/OdysseyKit/releases/download/v1.1.0-beta.26/Odyssey.xcframework.zip",// Odyssey URL
-            checksum: "21f9ba2fb6beac95d578802016372eff83fda4823c6aba1fa764a8544bd3ef46"// Odyssey Checksum
+            checksum: "7f56fc49c6bc36eca60bcd72a2a1169cfaea3bd5f178941eb62ace2991d333b1"// Odyssey Checksum
         ),
         .binaryTarget(
             name: "KMPNativeCoroutinesCore",
